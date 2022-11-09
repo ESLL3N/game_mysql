1a1a8ef20e823806ea5ed31db9b75bedd0d27f49
ESLL3N <esllenescolar@gmail.com>
2022-11-09 17:50:07 +0000

    jogos

---
+++ app/src/main/java/esllenlicurgo/controllers/JogosController.java
@@ -0,0 +1,14 @@
+package esllenlicurgo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+import esllenlicurgo.repositories.JogoRepository;
+
+@Controller
+public class JogosController {
+    private JogoRepository jogosRepo;
+
+    
+}
+
+
---
+++ app/src/main/java/esllenlicurgo/repositories/JogoRepository.java
@@ -0,0 +1,9 @@
+package esllenlicurgo.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import esllenlicurgo.models.Jogo;
+
+public interface JogoRepository extends CrudRepository<Jogo, Integer> {
+}
+
